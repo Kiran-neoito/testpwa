f9d9ce79ff49c743d3e73c698229775c51f3384d
Kiran <kiran.r@neoito.com>
2021-08-11 15:02:46 +0530

    fixes

---
+++ build/precache-manifest.4bfa19fc397fa9df87d8d4b902db3db7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f7dd4111c6362ad26a7ce1c8fda206",
+    "url": "/react-pwa-install-demo/index.html"
+  },
+  {
+    "revision": "40b7e2e31d98d46c0cea",
+    "url": "/react-pwa-install-demo/static/js/2.962fdad7.chunk.js"
+  },
+  {
+    "revision": "91a037b56ebf56b31d8dfeb265dcabdf",
+    "url": "/react-pwa-install-demo/static/js/2.962fdad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c756009145fbac00e3",
+    "url": "/react-pwa-install-demo/static/js/main.5e1b2366.chunk.js"
+  },
+  {
+    "revision": "6851b0cf0a58996db15d",
+    "url": "/react-pwa-install-demo/static/js/runtime-main.602f8855.js"
+  }
+]);
